9bc35036b4be52df659b9625e205ba32fcf964a5
heraldovalenti <heraldovalenti@gmail.com>
2014-01-17 15:39:46 -0300

    Initialization of project with Heraldo's files.

---
+++ TEG/src/controlador/movimiento/ControlMovimiento.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador.movimiento;
+
+import controlador.estructura.Paises;
+import modelo.juego.Pais;
+
+/**
+ *
+ * @author heril
+ */
+public class ControlMovimiento {
+
+    private Pais origen;
+    private Pais destino;
+    private int cantidadEjercitos;
+    private int cantidadMisiles;
+    private Movimiento movimiento;
+
+    public ControlMovimiento(Pais origen, Pais destino, int cantidadEjercitos, int cantidadMisiles) {
+        this.origen = origen;
+        this.destino = destino;
+        this.cantidadEjercitos = cantidadEjercitos;
+        this.cantidadMisiles = cantidadMisiles;
+    }
+
+    public int maximoMovimiento() {
+        return origen.getOcupacion().getEjercitos() - 1;
+    }
+
+    public boolean movimientoValido() {
+        if (!origen.getOcupacion().getJugador().equals(destino.getOcupacion().getJugador())) {
+            return false;
+        }
+        if (cantidadEjercitos > this.maximoMovimiento()) {
+            return false;
+        }
+        if (!Paises.sonLimitrofes(origen, destino)) {
+            return false;
+        }
+        return true;
+    }
+}
