debc213a403dd202fc29aaa9183bc63543f21c35
Daniel <Daniel@localhost>
2014-03-02 19:07:19 -0300

    La tercera es la vencida

--- TEG/src/juego/mecanicas/turno/GestorTurno.java
+++ TEG/src/juego/mecanicas/turno/GestorTurno.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package juego.mecanicas.turno;
 
 import juego.estructura.Pais;
@@ -13,38 +12,42 @@ import juego.estructura.Pais;
  * @author Daniel
  */
 public class GestorTurno {
+
     private static final int FUERA_TURNO = 0;
     private static final int ETAPA_INCORPORAR_EJERCITOS = 1;
     private static final int ETAPA_ATACAR = 2;
     private static final int ETAPA_REAGRUPAR = 3;
     private static final int ETAPA_SOLICITAR_TARJETA = 4;
-    
+
     private static final int ACCION_ATACAR = 1;
     private static final int ACCION_SOLICITAR_TARJETA = 1;
     private static final int ACCION_INCORPORAR_EJERCITOS = 1;
     private static final int ACCION_REAGRUPAR = 1;
     private static final int ACCION_CANJEAR_TARJETA = 1;
-    
+
     private static final boolean[][] permisos = new boolean[5][5];
-    
-    public void atacar(Pais origen, Pais destino, int cantidadEjercitos){
-        
+
+    public void atacar(Pais origen, Pais destino, int cantidadEjercitos) {
+
     }
-    public void colocarEjercitos(Pais pais){
-        
+
+    public void colocarEjercitos(Pais pais) {
+
     }
-    public void canjearEjercitosPorMisil(Pais pais){
-        
+
+    public void canjearEjercitosPorMisil(Pais pais) {
+
     }
-    public void canjearMisilPorEjercito(Pais pais){
-        
+
+    public void canjearMisilPorEjercito(Pais pais) {
+
     }
-    
-    public void reagruparEjercitos(Pais origen, Pais destino, int cantidadEjercitos){
-        
+
+    public void reagruparEjercitos(Pais origen, Pais destino, int cantidadEjercitos) {
+
     }
-    
-    public void lanzarMisil(Pais origen, Pais destino){
-        
+
+    public void lanzarMisil(Pais origen, Pais destino) {
+
     }
 }
