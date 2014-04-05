66312e29d14cdcee2d37f806fbf2b078e90240fd
Daniel <Daniel@localhost>
2014-04-05 05:25:00 -0300

    Cambios para el manejo de tarjetas y refuerzos. La IA deber?a poder ser capaz de realizar un canje.

---
+++ TEG/src/juego/mecanicas/turno/GestorTurno.java
@@ -0,0 +1,171 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juego.mecanicas.turno;
+
+import com.cliente.AccionableAtaque;
+import com.cliente.AccionableCanjePorEjercitos;
+import com.cliente.AccionableCanjePorMisil;
+import com.cliente.AccionableCanjeTarjetas;
+import com.cliente.AccionableLanzarMisil;
+import com.cliente.AccionableMovimiento;
+import cliente.ClienteManager;
+import com.cliente.AccionableRefuerzo;
+import com.cliente.AccionableSolicitarTarjeta;
+import java.util.List;
+import juego.Juego;
+import juego.estructura.Canjeable;
+import juego.estructura.GestorPaises;
+import juego.estructura.GestorTarjetas;
+import juego.estructura.Jugador;
+import juego.estructura.Pais;
+import juego.mecanicas.ataque.ControlAtaque;
+import juego.mecanicas.movimiento.ControlMovimiento;
+
+/**
+ *
+ * @author Daniel
+ */
+public class GestorTurno {
+
+    public static final int FUERA_TURNO = 0;
+    public static final int ETAPA_INCORPORAR_EJERCITOS = 1;
+    public static final int ETAPA_ATACAR = 2;
+    public static final int ETAPA_REAGRUPAR = 3;
+    public static final int ETAPA_SOLICITAR_TARJETA = 4;
+
+    public static final int ACCION_ATACAR = 1;
+    public static final int ACCION_LANZAR_MISIL = 2;
+    public static final int ACCION_SOLICITAR_TARJETA = 3;
+    public static final int ACCION_INCORPORAR_EJERCITOS = 4;
+    public static final int ACCION_REAGRUPAR = 5;
+    public static final int ACCION_CANJEAR_TARJETA = 6;
+    public static final int ACCION_CANJEAR_MISIL_POR_EJERCITO = 7;
+    public static final int ACCION_CANJEAR_EJERCITO_POR_MISIL = 8;
+
+    public static int etapaActual = 0;
+    private static int paisesConquistados = 0;
+    private static boolean canjeRealizado = false;
+    private static boolean tarjetaSolicitada = false;
+
+    private static boolean[][] permisos;
+
+    public static void crearPermisos() {
+        permisos = new boolean[5][8];
+
+        permisos[ETAPA_INCORPORAR_EJERCITOS][ACCION_INCORPORAR_EJERCITOS] = true;
+        permisos[ETAPA_INCORPORAR_EJERCITOS][ACCION_CANJEAR_EJERCITO_POR_MISIL] = true;
+        permisos[ETAPA_INCORPORAR_EJERCITOS][ACCION_CANJEAR_MISIL_POR_EJERCITO] = true;
+        permisos[ETAPA_INCORPORAR_EJERCITOS][ACCION_CANJEAR_TARJETA] = true;
+
+        permisos[ETAPA_ATACAR][ACCION_ATACAR] = true;
+        permisos[ETAPA_ATACAR][ACCION_LANZAR_MISIL] = true;
+
+        permisos[ETAPA_REAGRUPAR][ACCION_REAGRUPAR] = true;
+    }
+
+    public static void atacar(Pais origen, Pais destino) {
+        if (accionPermitida(ACCION_ATACAR)) {
+            ControlAtaque control = new ControlAtaque(origen, destino);
+            if (control.ataqueValido()) {
+                AccionableAtaque ataque = new AccionableAtaque(origen, destino);
+                ClienteManager.getInstance().registrarSalida(ataque);
+                etapaActual = ETAPA_ATACAR;
+            }
+        }
+    }
+
+//    public static void colocarEjercitos(Pais pais, int cantidadEjercitos, int cantidadMisiles) {
+//        if (accionPermitida(ACCION_INCORPORAR_EJERCITOS)) {
+//            AccionableRefuerzo refuerzo = new AccionableRefuerzo(pais, cantidadEjercitos, cantidadMisiles);
+//            ClienteManager.getInstance().registrarSalida(refuerzo);
+//        }
+//    }
+
+    public static void canjearEjercitosPorMisil(Pais pais, int cantidadMisiles) {
+        if (accionPermitida(ACCION_CANJEAR_EJERCITO_POR_MISIL)) {
+            if (pais.getCantidadEjercitos() > 6 * cantidadMisiles) {
+                AccionableCanjePorMisil canje = new AccionableCanjePorMisil(pais, cantidadMisiles);
+                ClienteManager.getInstance().registrarSalida(canje);
+            }
+        }
+    }
+
+    public static void canjearMisilPorEjercito(Pais pais, int cantidadMisiles) {
+        if (accionPermitida(ACCION_CANJEAR_MISIL_POR_EJERCITO)) {
+            if (pais.getCantidadMisiles() >= cantidadMisiles) {
+                AccionableCanjePorEjercitos canje = new AccionableCanjePorEjercitos(pais, cantidadMisiles);
+                ClienteManager.getInstance().registrarSalida(canje);
+            }
+        }
+
+    }
+
+    public static void reagruparEjercitos(Pais origen, Pais destino, int cantidadEjercitos, int cantidadMisiles) {
+        if (accionPermitida(ACCION_REAGRUPAR)) {
+            ControlMovimiento control = new ControlMovimiento(origen, destino, cantidadEjercitos, cantidadMisiles);
+            if (control.movimientoValido()) {
+                AccionableMovimiento movimiento = new AccionableMovimiento(origen, destino, cantidadEjercitos, cantidadMisiles);
+                ClienteManager.getInstance().registrarSalida(movimiento);
+                etapaActual = ETAPA_REAGRUPAR;
+            }
+        }
+    }
+
+    public static void lanzarMisil(Pais origen, Pais destino) {
+        if (accionPermitida(ACCION_LANZAR_MISIL)) {
+            if (origen.getCantidadMisiles() > destino.getCantidadMisiles() && GestorPaises.calcularDistancia(origen, destino) <= 3) {
+                AccionableLanzarMisil lanzamiento = new AccionableLanzarMisil(origen, destino);
+                ClienteManager.getInstance().registrarSalida(lanzamiento);
+                etapaActual = ETAPA_ATACAR;
+            }
+        }
+    }
+
+    public static void canjearTarjetas(Jugador jugador, List<Canjeable> listaTarjetas) {
+        if (accionPermitida(ACCION_CANJEAR_TARJETA) && !canjeRealizado) {
+            if (GestorTarjetas.canjeValido(jugador, listaTarjetas)) {
+                AccionableCanjeTarjetas canje = new AccionableCanjeTarjetas(jugador, listaTarjetas);
+                ClienteManager.getInstance().registrarSalida(canje);
+                canjeRealizado = true;
+            }
+        }
+    }
+
+    public static void solicitarTarjeta(Jugador jugador) {
+        if (accionPermitida(ACCION_SOLICITAR_TARJETA) && !tarjetaSolicitada && jugador.getCantidadTarjetasPais() < 6 && Juego.getInstancia().getSituacion().puedeObtenerTarjetaPais(jugador)) {
+            int canjesRealizados = jugador.getCantidadCanjes();
+            boolean res = false;
+            if (canjesRealizados < 3 && paisesConquistados > 0) {
+                res = true;
+            }
+            if (canjesRealizados > 3 && paisesConquistados > 1) {
+                res = true;
+            }
+            if (res) {
+                AccionableSolicitarTarjeta solicitar = new AccionableSolicitarTarjeta(jugador);
+                ClienteManager.getInstance().registrarSalida(solicitar);
+                tarjetaSolicitada = true;
+            }
+        }
+    }
+
+    public static boolean accionPermitida(int accion) {
+        return permisos[etapaActual][accion];
+    }
+
+    public static void finTurno() {
+        etapaActual = FUERA_TURNO;
+        paisesConquistados = 0;
+        canjeRealizado = false;
+        tarjetaSolicitada = false;
+    }
+
+    public static void aumentarContadorPaisesConquistados() {
+        paisesConquistados++;
+    }
+    
+
+}
---
+++ TEG/src/servidor/GestorClientes.java
@@ -0,0 +1,279 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servidor;
+
+import com.Accionable;
+import com.cliente.AccionableCerrarConexion;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
+import servidor.control.ControlInicioConexion;
+
+/**
+ *
+ * @author heril
+ */
+public class GestorClientes implements Runnable {
+
+    private Thread hilo;
+    private LinkedList<ConexionCliente> conexionesCliente;
+    private ConexionCliente conexionAAtender;
+    private ColaAcciones colaAcciones;
+    private boolean banderaEjecucion;
+    private static final int maximoClientes = 6;
+
+    public GestorClientes(ColaAcciones colaAcciones) {
+        conexionesCliente = new LinkedList();
+        conexionAAtender = null;
+        this.colaAcciones = colaAcciones;
+        banderaEjecucion = false;
+    }
+
+    /**
+     * Agrega una conexion con un cliente al gestor. Ejecuta el procedimiento de
+     * inicio de conexion con el cliente. Si la sala esta llena, no se almacena
+     * la conexion y envia la orden de cerrar la conexion al cliente, informando
+     * ademas la situacion.
+     *
+     * @param cc ConexionCliente a agregar
+     */
+    public void agregarCliente(ConexionCliente cc) {
+        cc.setId(generarIdentificadorUnico());
+        if (conexionesCliente.size() < maximoClientes) {
+            conexionesCliente.add(cc);
+            new ControlInicioConexion(cc).ejecutar();
+        } else {
+            new ControlInicioConexion(cc).finalizarConexion();
+        }
+    }
+
+    /**
+     * Quita una conexion de la coleccion de conexiones a clientes.
+     *
+     * @param idCliente el id de la conexion a quitarse.
+     * @return la conexion identificada, null si no se ha encontrado ninguna
+     * conexion.
+     */
+    public ConexionCliente quitarCliente(int idCliente) {
+        ConexionCliente res = null;
+        for (int i = 0; i < conexionesCliente.size(); i++) {
+            ConexionCliente cc = conexionesCliente.get(i);
+            if (cc.getId() == idCliente) {
+                res = conexionesCliente.remove(i);
+                break;
+            }
+        }
+        return res;
+    }
+
+    /**
+     * Genera un identificador unico para los nuevos clientes que se conectan al
+     * servidor. Para realizarlo, genera un numero aleatorio y verifica que los
+     * clientes ya conectados no contengan el mismo identificador.
+     *
+     * @return int el identificador generado.
+     */
+    private int generarIdentificadorUnico() {
+        boolean identificadorValido;
+        int identificador = -1;
+        do {
+            double rnd = Math.random();
+            identificador = (int) (rnd * 1000000000);
+            boolean forInterrumpido = false;
+            for (ConexionCliente cc : conexionesCliente) {
+                if (cc.getId() == identificador || identificador == 0) {
+                    forInterrumpido = true;
+                    break;
+                }
+            }
+            if (forInterrumpido) {
+                identificadorValido = false;
+            } else {
+                identificadorValido = true;
+            }
+        } while (!identificadorValido);
+        return identificador;
+    }
+
+    /**
+     * Metodo para consultar si hay datos disponibles enviados por algun
+     * cliente. Establece la conexion a atender del cliente que ha enviado datos
+     * y estan listos para recibirse.
+     *
+     * @return true si hay datos para leer, false en caso contrario.
+     */
+    private boolean datosDisponibles() {
+        conexionAAtender = checkDatosDisponibles();
+        rotarColaConexiones();
+        return conexionAAtender != null;
+    }
+
+    /**
+     * Rota la cola de conexiones establecidas con clientes. La conexion que
+     * estaba en primer lugar de la cola, pasa a estar en ultimo lugar ahora,
+     * con el objetivo de que si un cliente envia datos constantemente, los
+     * demas clientes tambien tengan oportunidad de ser atendidos por el
+     * servidor.
+     */
+    private void rotarColaConexiones() {
+        ConexionCliente aux = conexionesCliente.pollFirst();
+        if (aux != null) {
+            conexionesCliente.addLast(aux);
+        }
+    }
+
+    /**
+     * Metodo para obtener los accionables enviados por los clientes. Lee un
+     * accionable del cliente indicado por el la conexion a servir y lo agrega a
+     * la cola de acciones. Si la conexion es null no hace nada.
+     *
+     * @return Accionable enviando por el cliente.
+     */
+    private void leerAccionable() {
+        if (conexionAAtender == null) {
+            return;
+        }
+        Accionable accion = recibir();
+        colaAcciones.solicitarAcceso();
+        colaAcciones.pushEntrada(accion);
+        colaAcciones.informarSalida();
+        conexionAAtender = null;
+    }
+
+    /**
+     * Envia un accionable a todos los clientes conectados. Obtiene el
+     * accionable a enviarse desde la cola de salida de acciones.
+     *
+     * @param accion el accionable a enviarse.
+     */
+    private void enviarAccionable() {
+        colaAcciones.solicitarAcceso();
+        Accionable accion = colaAcciones.pullSalida();
+        colaAcciones.informarSalida();
+        for (ConexionCliente aux : conexionesCliente) {
+            aux.enviar(accion);
+        }
+    }
+
+    /**
+     * Envia un accionable a todos los clientes conectados.
+     *
+     * @param accion el accionable a enviarse.
+     */
+    public void enviarAccionable(Accionable accion) {
+        for (ConexionCliente aux : conexionesCliente) {
+            aux.enviar(accion);
+        }
+    }
+
+    /**
+     * Envia un accionable a la conexion cliente especificada. Sobrecarga para
+     * enviar mensajes a un cliente en particular.
+     *
+     * @param accion el accionable a enviarse.
+     * @param cc la conexion a enviarse el accionable.
+     */
+    public void enviarAccionable(Accionable accion, ConexionCliente cc) {
+        int indexOf = conexionesCliente.indexOf(cc);
+        conexionesCliente.get(indexOf).enviar(accion);
+    }
+
+    /**
+     * Chequea si hay datos para recibir de los clientes.
+     *
+     * @return la conexion de cliente que envio datos, null si no existen datos.
+     */
+    private ConexionCliente checkDatosDisponibles() {
+        for (ConexionCliente cc : conexionesCliente) {
+            if (cc.disponible() != 0) {
+                return cc;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Devuelve lee un accionable enviado por el cliente.
+     *
+     * @return Accionable la accion enviada por el cliente.
+     */
+    private Accionable recibir() {
+        return conexionAAtender.recibir();
+    }
+
+    /**
+     * Verifica la existencia de salidas esperando para ser enviadas.
+     *
+     * @return true si hay salidas pendientes de envio, false en otro caso.
+     */
+    private boolean salidasEnEspera() {
+        colaAcciones.solicitarAcceso();
+        boolean res = colaAcciones.haySalidas();
+        colaAcciones.informarSalida();
+        return res;
+    }
+
+    /**
+     * Indica que el gestor debe parar su ejecucion y cierra las conexiones con
+     * clientes activas.
+     */
+    public void parar() {
+        banderaEjecucion = false;
+        enviarAccionable(new AccionableCerrarConexion());
+        conexionesCliente.clear();
+    }
+
+    /**
+     * Informa si hay conexiones establecidas con clientes.
+     *
+     * @return true si hay alguna conexion establecida, false en otro caso.
+     */
+    public boolean conexionesEstablecidas() {
+        return conexionesCliente.size() != 0;
+    }
+    
+    /**
+     * Informa la cantidad de conexiones establecidas con clientes.
+     * @return la cantidad de conexiones establecidas.
+     */
+    public int cantidadConexionesEstablecidas() {
+        return conexionesCliente.size();
+    }
+
+    /**
+     * Informa el identificador de las conexiones establecidas con clientes.
+     *
+     * @return un conjunto con los id de las conexiones establecidas.
+     */
+    public Set<Integer> getIdConexionesEstablecidas() {
+        Set<Integer> res = new HashSet<>();
+        for (ConexionCliente cc : conexionesCliente) {
+            res.add(cc.getId());
+        }
+        return res;
+    }
+
+    @Override
+    public void run() {
+        banderaEjecucion = true;
+        while (banderaEjecucion) {
+            if (datosDisponibles()) {
+                leerAccionable();
+            }
+            if (salidasEnEspera()) {
+                enviarAccionable();
+            }
+            try {
+                hilo.sleep(conf.Configuracion.getInstancia().tiempoEspera());
+            } catch (InterruptedException ex) {
+                System.err.println("Error tratando de dormir: " + ex.getMessage());
+            }
+        }
+    }
+
+    public void setHilo(Thread hilo) {
+        this.hilo = hilo;
+    }
+}
