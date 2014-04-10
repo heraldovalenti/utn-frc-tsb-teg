abdbb2a79305f9a64b1b9947ce1dec57614609ac
broggi <broggi@localhost>
2014-04-10 04:57:42 -0300

    10 tarjetas

--- TEG/src/Interfaces/Tarjetas.java
+++ TEG/src/Interfaces/Tarjetas.java
@@ -24,10 +24,11 @@ public class Tarjetas extends javax.swing.JInternalFrame {
     private JMenuItem menu;
     private JButton boton;
     private List<Canjeable> tarjetasSeleccionadas = new ArrayList<Canjeable>();    
+    private boolean canjePermitido;
     /**
      * Creates new form Tarjetas
      */
-    public Tarjetas(List<Canjeable> tarjetas, JMenuItem menu, JButton boton) {
+    public Tarjetas(List<Canjeable> tarjetas, JMenuItem menu, JButton boton, boolean canjePermitido) {
         initComponents();
         this.tarjetas = (ArrayList)tarjetas;
         limpiarLabels();
@@ -35,28 +36,43 @@ public class Tarjetas extends javax.swing.JInternalFrame {
         this.menu = menu;
         deshabilitarChecks();
         btnCanjear.setEnabled(false);
-        if(FachadaInterface.esMiTurno(FachadaInterface.getJugadorLocal())){
-             cargarTarjetas();   
-        }    
+        this.canjePermitido = canjePermitido;
+        cargarTarjetas();   
+  
     }
     private void cargarTarjetas(){
-        if(tarjetas == null) return;
+        if(tarjetas == null) return;        
         for(int i = 0; i<tarjetas.size(); i++){
             switch(i){
                 case(0): cargarTarjeta(lblTarjeta1,tarjetas.get(0).getNombre());
-                         chkTarjeta1.setEnabled(true);
+                         chkTarjeta1.setEnabled(canjePermitido);
                          break;
                 case(1): cargarTarjeta(lblTarjeta2,tarjetas.get(1).getNombre());
-                         chkTarjeta2.setEnabled(true);
+                         chkTarjeta2.setEnabled(canjePermitido);
                          break;
                 case(2): cargarTarjeta(lblTarjeta3,tarjetas.get(2).getNombre());
-                         chkTarjeta3.setEnabled(true);
+                         chkTarjeta3.setEnabled(canjePermitido);
                          break;
                 case(3): cargarTarjeta(lblTarjeta4,tarjetas.get(3).getNombre());
-                         chkTarjeta4.setEnabled(true);
+                         chkTarjeta4.setEnabled(canjePermitido);
                          break;
                 case(4): cargarTarjeta(lblTarjeta5,tarjetas.get(4).getNombre());    
-                         chkTarjeta5.setEnabled(true);
+                         chkTarjeta5.setEnabled(canjePermitido);
+                         break;
+                case(5): cargarTarjeta(lblTarjeta6,tarjetas.get(5).getNombre());    
+                         chkTarjeta6.setEnabled(canjePermitido);
+                         break;
+                case(6): cargarTarjeta(lblTarjeta7,tarjetas.get(6).getNombre());    
+                         chkTarjeta7.setEnabled(canjePermitido);
+                         break;
+                case(7): cargarTarjeta(lblTarjeta8,tarjetas.get(7).getNombre());    
+                         chkTarjeta8.setEnabled(canjePermitido);
+                         break;
+                case(8): cargarTarjeta(lblTarjeta9,tarjetas.get(8).getNombre());    
+                         chkTarjeta9.setEnabled(canjePermitido);
+                         break;
+                case(9): cargarTarjeta(lblTarjeta10,tarjetas.get(9).getNombre());    
+                         chkTarjeta10.setEnabled(canjePermitido);
                          break;
             }
         }
@@ -67,6 +83,11 @@ public class Tarjetas extends javax.swing.JInternalFrame {
         chkTarjeta3.setEnabled(false);
         chkTarjeta4.setEnabled(false);
         chkTarjeta5.setEnabled(false);
+        chkTarjeta6.setEnabled(false);
+        chkTarjeta7.setEnabled(false);
+        chkTarjeta8.setEnabled(false);
+        chkTarjeta9.setEnabled(false);
+        chkTarjeta10.setEnabled(false);
     }
     private void cargarTarjeta(JLabel label, String pais){
         try{
@@ -82,6 +103,11 @@ public class Tarjetas extends javax.swing.JInternalFrame {
         lblTarjeta3.setIcon(null);
         lblTarjeta4.setIcon(null);
         lblTarjeta5.setIcon(null);
+        lblTarjeta6.setIcon(null);
+        lblTarjeta7.setIcon(null);
+        lblTarjeta8.setIcon(null);
+        lblTarjeta9.setIcon(null);
+        lblTarjeta10.setIcon(null);
     }
     private boolean puedoCanjear(){
         return FachadaInterface.canjeValido(FachadaInterface.getJugadorLocal(),tarjetasSeleccionadas);
@@ -141,18 +167,26 @@ public class Tarjetas extends javax.swing.JInternalFrame {
         chkTarjeta2 = new javax.swing.JCheckBox();
         chkTarjeta3 = new javax.swing.JCheckBox();
         chkTarjeta4 = new javax.swing.JCheckBox();
+        chkTarjeta9 = new javax.swing.JCheckBox();
+        chkTarjeta8 = new javax.swing.JCheckBox();
+        chkTarjeta7 = new javax.swing.JCheckBox();
+        chkTarjeta6 = new javax.swing.JCheckBox();
+        chkTarjeta10 = new javax.swing.JCheckBox();
+        jPanel8 = new javax.swing.JPanel();
+        lblTarjeta10 = new javax.swing.JLabel();
+        jPanel9 = new javax.swing.JPanel();
+        lblTarjeta9 = new javax.swing.JLabel();
+        jPanel10 = new javax.swing.JPanel();
+        lblTarjeta8 = new javax.swing.JLabel();
+        jPanel11 = new javax.swing.JPanel();
+        lblTarjeta7 = new javax.swing.JLabel();
+        jPanel3 = new javax.swing.JPanel();
+        lblTarjeta6 = new javax.swing.JLabel();
 
         setClosable(true);
         setTitle("Canje De Tarjetas");
         setFrameIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/canje.png"))); // NOI18N
         addInternalFrameListener(new javax.swing.event.InternalFrameListener() {
-            public void internalFrameClosed(javax.swing.event.InternalFrameEvent evt) {
-            }
-            public void internalFrameClosing(javax.swing.event.InternalFrameEvent evt) {
-                formInternalFrameClosing(evt);
-            }
-            public void internalFrameOpened(javax.swing.event.InternalFrameEvent evt) {
-            }
             public void internalFrameActivated(javax.swing.event.InternalFrameEvent evt) {
             }
             public void internalFrameDeactivated(javax.swing.event.InternalFrameEvent evt) {
@@ -160,6 +194,13 @@ public class Tarjetas extends javax.swing.JInternalFrame {
             public void internalFrameDeiconified(javax.swing.event.InternalFrameEvent evt) {
             }
             public void internalFrameIconified(javax.swing.event.InternalFrameEvent evt) {
+            }
+            public void internalFrameClosed(javax.swing.event.InternalFrameEvent evt) {
+            }
+            public void internalFrameClosing(javax.swing.event.InternalFrameEvent evt) {
+                formInternalFrameClosing(evt);
+            }
+            public void internalFrameOpened(javax.swing.event.InternalFrameEvent evt) {
             }
         });
 
@@ -178,7 +219,7 @@ public class Tarjetas extends javax.swing.JInternalFrame {
                 btnCanjearActionPerformed(evt);
             }
         });
-        jPanel1.add(btnCanjear, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 230, -1, -1));
+        jPanel1.add(btnCanjear, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 410, -1, -1));
 
         jPanel2.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
         jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -255,6 +296,81 @@ public class Tarjetas extends javax.swing.JInternalFrame {
         });
         jPanel1.add(chkTarjeta4, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 200, -1, -1));
 
+        chkTarjeta9.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                chkTarjeta9ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(chkTarjeta9, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 380, -1, -1));
+
+        chkTarjeta8.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                chkTarjeta8ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(chkTarjeta8, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 380, -1, -1));
+
+        chkTarjeta7.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                chkTarjeta7ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(chkTarjeta7, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 380, -1, -1));
+
+        chkTarjeta6.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                chkTarjeta6ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(chkTarjeta6, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 380, -1, -1));
+
+        chkTarjeta10.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                chkTarjeta10ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(chkTarjeta10, new org.netbeans.lib.awtextra.AbsoluteConstraints(540, 380, -1, -1));
+
+        jPanel8.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        jPanel8.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        lblTarjeta10.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/Tarjetas/alaska.png"))); // NOI18N
+        jPanel8.add(lblTarjeta10, new org.netbeans.lib.awtextra.AbsoluteConstraints(1, 1, 98, -1));
+
+        jPanel1.add(jPanel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 230, 100, 147));
+
+        jPanel9.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        jPanel9.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        lblTarjeta9.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/Tarjetas/alaska.png"))); // NOI18N
+        jPanel9.add(lblTarjeta9, new org.netbeans.lib.awtextra.AbsoluteConstraints(1, 1, 98, -1));
+
+        jPanel1.add(jPanel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 230, 100, 147));
+
+        jPanel10.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        jPanel10.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        lblTarjeta8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/Tarjetas/alaska.png"))); // NOI18N
+        jPanel10.add(lblTarjeta8, new org.netbeans.lib.awtextra.AbsoluteConstraints(1, 1, 98, -1));
+
+        jPanel1.add(jPanel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 230, 100, 147));
+
+        jPanel11.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        jPanel11.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        lblTarjeta7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/Tarjetas/alaska.png"))); // NOI18N
+        jPanel11.add(lblTarjeta7, new org.netbeans.lib.awtextra.AbsoluteConstraints(1, 1, 98, -1));
+
+        jPanel1.add(jPanel11, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 230, 100, 147));
+
+        jPanel3.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        jPanel3.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        lblTarjeta6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/Tarjetas/alaska.png"))); // NOI18N
+        jPanel3.add(lblTarjeta6, new org.netbeans.lib.awtextra.AbsoluteConstraints(1, 1, 98, -1));
+
+        jPanel1.add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 230, 100, 147));
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -263,9 +379,7 @@ public class Tarjetas extends javax.swing.JInternalFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 260, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 11, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 447, Short.MAX_VALUE)
         );
 
         pack();
@@ -337,24 +451,94 @@ public class Tarjetas extends javax.swing.JInternalFrame {
         }
     }//GEN-LAST:event_chkTarjeta4ActionPerformed
 
+    private void chkTarjeta9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_chkTarjeta9ActionPerformed
+        if(chkTarjeta9.isSelected()){
+            if(!cargarTarjetaSeleccionada(tarjetas.get(8))){
+                chkTarjeta9.setSelected(false);                
+            }
+        }
+        else{
+            quitarTarjetasSelccionada(tarjetas.get(8));
+        }
+    }//GEN-LAST:event_chkTarjeta9ActionPerformed
+
+    private void chkTarjeta8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_chkTarjeta8ActionPerformed
+        if(chkTarjeta8.isSelected()){
+            if(!cargarTarjetaSeleccionada(tarjetas.get(7))){
+                chkTarjeta8.setSelected(false);                
+            }
+        }
+        else{
+            quitarTarjetasSelccionada(tarjetas.get(7));
+        }
+    }//GEN-LAST:event_chkTarjeta8ActionPerformed
+
+    private void chkTarjeta7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_chkTarjeta7ActionPerformed
+        if(chkTarjeta7.isSelected()){
+            if(!cargarTarjetaSeleccionada(tarjetas.get(6))){
+                chkTarjeta7.setSelected(false);                
+            }
+        }
+        else{
+            quitarTarjetasSelccionada(tarjetas.get(6));
+        }
+    }//GEN-LAST:event_chkTarjeta7ActionPerformed
+
+    private void chkTarjeta6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_chkTarjeta6ActionPerformed
+        if(chkTarjeta6.isSelected()){
+            if(!cargarTarjetaSeleccionada(tarjetas.get(5))){
+                chkTarjeta6.setSelected(false);                
+            }
+        }
+        else{
+            quitarTarjetasSelccionada(tarjetas.get(5));
+        }
+    }//GEN-LAST:event_chkTarjeta6ActionPerformed
+
+    private void chkTarjeta10ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_chkTarjeta10ActionPerformed
+        if(chkTarjeta10.isSelected()){
+            if(!cargarTarjetaSeleccionada(tarjetas.get(9))){
+                chkTarjeta10.setSelected(false);                
+            }
+        }
+        else{
+            quitarTarjetasSelccionada(tarjetas.get(9));
+        }
+    }//GEN-LAST:event_chkTarjeta10ActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnCanjear;
     private javax.swing.JCheckBox chkTarjeta1;
+    private javax.swing.JCheckBox chkTarjeta10;
     private javax.swing.JCheckBox chkTarjeta2;
     private javax.swing.JCheckBox chkTarjeta3;
     private javax.swing.JCheckBox chkTarjeta4;
     private javax.swing.JCheckBox chkTarjeta5;
+    private javax.swing.JCheckBox chkTarjeta6;
+    private javax.swing.JCheckBox chkTarjeta7;
+    private javax.swing.JCheckBox chkTarjeta8;
+    private javax.swing.JCheckBox chkTarjeta9;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel10;
+    private javax.swing.JPanel jPanel11;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
     private javax.swing.JPanel jPanel5;
     private javax.swing.JPanel jPanel6;
     private javax.swing.JPanel jPanel7;
+    private javax.swing.JPanel jPanel8;
+    private javax.swing.JPanel jPanel9;
     private javax.swing.JLabel lblTarjeta1;
+    private javax.swing.JLabel lblTarjeta10;
     private javax.swing.JLabel lblTarjeta2;
     private javax.swing.JLabel lblTarjeta3;
     private javax.swing.JLabel lblTarjeta4;
     private javax.swing.JLabel lblTarjeta5;
+    private javax.swing.JLabel lblTarjeta6;
+    private javax.swing.JLabel lblTarjeta7;
+    private javax.swing.JLabel lblTarjeta8;
+    private javax.swing.JLabel lblTarjeta9;
     // End of variables declaration//GEN-END:variables
 }
